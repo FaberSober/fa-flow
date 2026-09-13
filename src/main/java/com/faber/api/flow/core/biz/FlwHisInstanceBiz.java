e07907bf9edf8ea05eec561d8a71a0a4695aa109
xu.pengfei <1508075252@qq.com>
2026-09-13 22:12:09 +0800

    实现 FlwHisInstanceBiz 类的删除功能，添加 removeById 方法以支持根据 ID 删除历史流程实例及相关任务

--- src/main/java/com/faber/api/flow/core/biz/FlwHisInstanceBiz.java
+++ src/main/java/com/faber/api/flow/core/biz/FlwHisInstanceBiz.java
@@ -1,10 +1,19 @@
 package com.faber.api.flow.core.biz;
 
-import org.springframework.stereotype.Service;
+import java.io.Serializable;
+import java.util.Collections;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.aizuda.bpm.engine.dao.FlwExtInstanceDao;
+import com.aizuda.bpm.engine.dao.FlwHisTaskActorDao;
+import com.aizuda.bpm.engine.dao.FlwHisTaskDao;
 import com.faber.api.flow.core.entity.FaFlwHisInstance;
 import com.faber.api.flow.core.mapper.FaFlwHisInstanceMapper;
 import com.faber.core.web.biz.BaseBiz;
+
+import jakarta.annotation.Resource;
 
 /**
  * 历史流程实例表
@@ -15,4 +24,23 @@ import com.faber.core.web.biz.BaseBiz;
  */
 @Service
 public class FlwHisInstanceBiz extends BaseBiz<FaFlwHisInstanceMapper, FaFlwHisInstance> {
+
+    @Resource FlwExtInstanceDao flwExtInstanceDao;
+    @Resource FlwHisTaskDao flwHisTaskDao;
+    @Resource FlwHisTaskActorDao flwHisTaskActorDao;
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public boolean removeById(Serializable id) {
+        Long instanceId = toLong(id);
+        flwHisTaskActorDao.deleteByInstanceIds(Collections.singletonList(instanceId));
+        flwHisTaskDao.deleteByInstanceIds(Collections.singletonList(instanceId));
+        flwExtInstanceDao.deleteById(instanceId);
+        return super.removeById(id);
+    }
+
+    private Long toLong(Serializable id) {
+        if (id instanceof Number number) return number.longValue();
+        return Long.valueOf(id.toString());
+    }
 }
